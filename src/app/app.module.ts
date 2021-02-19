643b1094ed8b9d8d243cd74709295e72f4e93cc3
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-19 21:14:39 +0100

    chore: rename project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +14,7 @@ import { CommonComponentsModule } from './common-components/common-components.mo
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    HammerModule,
 
     CommonComponentsModule,
 
