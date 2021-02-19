42972daf9a1d9146c29e488ae758bbc4b3f82e3c
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-19 21:11:02 +0100

    ref: move navigation to own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,23 @@
-import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { CommonComponentsModule } from './common-components/common-components.module';
 
 @NgModule({
-  declarations: [AppComponent, MainNavigationComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+
+    CommonComponentsModule,
+
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
