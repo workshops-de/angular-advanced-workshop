4323edb78f3b2a123987034efc3f1ba329897769
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-19 21:54:29 +0100

    chore: migrate to eslint

--- src/app/book/models/book-empty.ts
+++ src/app/book/models/book-empty.ts
@@ -1,18 +1,16 @@
 import { Book } from './book';
 
-export function emptyBook(): Book {
-  return {
-    id: '',
-    title: '',
-    subtitle: '',
-    cover: '',
-    isbn: '',
-    abstract: '',
-    numPages: 0,
-    author: '',
-    publisher: {
-      name: '',
-      url: ''
-    }
-  };
-}
+export const emptyBook: () => Book = () => ({
+  id: '',
+  title: '',
+  subtitle: '',
+  cover: '',
+  isbn: '',
+  abstract: '',
+  numPages: 0,
+  author: '',
+  publisher: {
+    name: '',
+    url: ''
+  }
+});
