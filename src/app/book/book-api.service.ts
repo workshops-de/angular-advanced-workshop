2d805cb75c76563f53738420e1cdd61eef78a9fb
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-21 14:18:26 +0100

    solve--testing-service-with-http

--- src/app/book/book-api.service.ts
+++ src/app/book/book-api.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Book } from './models';
 
 @Injectable({ providedIn: 'root' })
@@ -10,7 +11,15 @@ export class BookApiService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.endpoint}`);
+    return this.http
+      .get<Book[]>(`${this.endpoint}`)
+      .pipe(
+        catchError((err: HttpErrorResponse) =>
+          err.status === 500
+            ? throwError(new Error('Sorry, we could not load any books'))
+            : throwError(new Error('Sorry, we have connectivity issues.'))
+        )
+      );
   }
 
   getByIsbn(isbn: string): Observable<Book> {
