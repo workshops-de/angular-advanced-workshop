12e2c9161ecf9ed1586ec634941936614693cf2c
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-21 10:56:41 +0100

    solve--testing-component-template

--- src/app/book/book-card/book-card.component.spec.ts
+++ src/app/book/book-card/book-card.component.spec.ts
@@ -1,3 +1,8 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Book, bookNa } from '../models';
 import { BookCardComponent } from './book-card.component';
 
 describe('<ws-book-card>', () => {
@@ -10,6 +15,46 @@ describe('<ws-book-card>', () => {
         expect(component.content.title).toBe(na);
         expect(component.content.subtitle).toBe(na);
         expect(component.content.author).toBe(na);
+      });
+    });
+  });
+
+  describe('template', () => {
+    let fixture: ComponentFixture<BookCardComponent>;
+    let book: Book;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [BookCardComponent],
+        imports: [MatCardModule, RouterTestingModule]
+      });
+
+      fixture = TestBed.createComponent(BookCardComponent);
+      fixture.detectChanges();
+
+      book = {
+        ...bookNa(),
+        title: 'My book'
+      };
+    });
+
+    describe('When content is passed', () => {
+      it('renders the content', () => {
+        fixture.componentInstance.content = book;
+        fixture.detectChanges();
+
+        const titleFixture = fixture.debugElement.query(By.css('[data-test=book-card-title]'));
+        const titleElement: HTMLElement = titleFixture.nativeElement;
+
+        expect(titleElement.innerHTML).toBe(book.title);
+      });
+    });
+
+    describe('When no content is passed', () => {
+      it('renders "n/a"', () => {
+        const titleFixture = fixture.debugElement.query(By.css('[data-test=book-card-title]'));
+        const titleElement: HTMLElement = titleFixture.nativeElement;
+
+        expect(titleElement.innerHTML).toBe('n/a');
       });
     });
   });
