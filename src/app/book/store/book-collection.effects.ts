2c239b65ecf55be2a9958dc198f1aefa735799be
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-24 11:12:27 +0100

    solve--ngrx-use-effects-everywhere

--- src/app/book/store/book-collection.effects.ts
+++ src/app/book/store/book-collection.effects.ts
@@ -3,7 +3,16 @@ import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { exhaustMap, map, switchMap } from 'rxjs/operators';
 import { BookApiService } from '../book-api.service';
-import { createBookComplete, createBookStart, loadBooksComplete, loadBooksStart } from './book-collection.actions';
+import {
+  createBookComplete,
+  createBookStart,
+  deleteBookComplete,
+  deleteBookStart,
+  loadBooksComplete,
+  loadBooksStart,
+  updateBookComplete,
+  updateBookStart
+} from './book-collection.actions';
 
 @Injectable()
 export class BookCollectionEffects {
@@ -22,11 +31,26 @@ export class BookCollectionEffects {
       map(bookCreated => createBookComplete({ book: bookCreated }))
     )
   );
+  delete = createEffect(() =>
+    this.actions$.pipe(
+      ofType(deleteBookStart),
+      exhaustMap(({ bookIsbn }) => this.bookApi.delete(bookIsbn).pipe(map(() => deleteBookComplete({ bookIsbn }))))
+    )
+  );
+
+  update = createEffect(() =>
+    this.actions$.pipe(
+      ofType(updateBookStart),
+      exhaustMap(({ patch }) =>
+        this.bookApi.update(patch.isbn, patch).pipe(map(update => updateBookComplete({ update })))
+      )
+    )
+  );
 
   navigateToStart = createEffect(
     () =>
       this.actions$.pipe(
-        ofType(createBookComplete),
+        ofType(createBookComplete, deleteBookComplete, updateBookComplete),
         switchMap(() => this.router.navigateByUrl('/'))
       ),
     { dispatch: false }
