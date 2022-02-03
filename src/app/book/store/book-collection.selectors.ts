web-dave/angular-advanced-workshop-1

solve--ngrx-use-entity

src/app/book/store/book-collection.selectors.ts
@@ -1,8 +1,14 @@
+import { createEntityAdapter } from '@ngrx/entity';
 import { createSelector } from '@ngrx/store';
 import { selectRouteParam } from '@store/router';
+import { Book } from '../models';
 import { bookFeature } from './book.feature';
 
-export const bookCollection = createSelector(bookFeature, slice => slice.bookCollection.entities);
+const adapter = createEntityAdapter<Book>();
+
+const bookCollectionSelector = createSelector(bookFeature, slice => slice.bookCollection);
+
+export const { selectAll: bookCollection } = adapter.getSelectors(bookCollectionSelector);
 
 export const bookByIsbn = createSelector(selectRouteParam('isbn'), bookCollection, (isbn, books) =>
   books.find(book => book.isbn === isbn)

workshops-de/angular-advanced-workshop

src/app/book/store/book-collection.selectors.ts
@@ -1,10 +1,14 @@
+import { createEntityAdapter } from '@ngrx/entity';
 import { createSelector } from '@ngrx/store';
 import { selectRouteParam } from '@store/router';
+import { Book } from '../models';
 import { bookFeature } from './book.feature';
+
+const adapter = createEntityAdapter<Book>();
 
 const bookCollectionSliceSelector = createSelector(bookFeature, feature => feature.bookCollection);
 
-export const bookCollection = createSelector(bookCollectionSliceSelector, slice => slice.entities);
+export const { selectAll: bookCollection } = adapter.getSelectors(bookCollectionSliceSelector);
 
 export const bookByIsbn = createSelector(selectRouteParam('isbn'), bookCollection, (isbn, books) =>
   books.find(book => book.isbn === isbn)