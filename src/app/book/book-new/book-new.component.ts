2ad13be993409252cf6f70d7baa1e7fe063db0ec
web-dave <dave@webdave.de>
2022-12-02 11:33:02 +0100

    pre 15

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -14,6 +14,7 @@ import { bookNa } from '../models';
 export class BookNewComponent implements OnDestroy {
   sink = new Subscription();
   form: FormGroup;
+  ctrl = new FormControl();
 
   constructor(private router: Router, private fb: FormBuilder, private bookService: BookApiService) {
     this.form = this.buildForm();
