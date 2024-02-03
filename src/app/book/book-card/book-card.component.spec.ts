a1f3e5729c7ecc544d70554cebda730d0c84aa8d
Christian Schulz <kontakt@schulzolutions.de>
2024-02-03 15:42:41 +0100

    solve--testing-component-template

--- src/app/book/book-card/book-card.component.spec.ts
+++ src/app/book/book-card/book-card.component.spec.ts
@@ -1,4 +1,8 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Book, bookNa } from '../models';
 import { BookCardComponent } from './book-card.component';
+import { provideRouter } from '@angular/router';
 
 describe('<ws-book-card>', () => {
   describe('unit', () => {
@@ -10,6 +14,46 @@ describe('<ws-book-card>', () => {
         expect(component.content.title).toBe(na);
         expect(component.content.subtitle).toBe(na);
         expect(component.content.author).toBe(na);
+      });
+    });
+  });
+
+  describe('template', () => {
+    let fixture: ComponentFixture<BookCardComponent>;
+    let book: Book;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [BookCardComponent],
+        providers: [provideRouter([])]
+      });
+
+      fixture = TestBed.createComponent(BookCardComponent);
+      fixture.detectChanges();
+
+      book = {
+        ...bookNa(),
+        title: 'My book'
+      };
+    });
+
+    describe('When content is passed', () => {
+      it('renders the content', () => {
+        fixture.componentRef.setInput('content', book);
+        fixture.detectChanges();
+
+        const titleFixture = fixture.debugElement.query(By.css('mat-card-title'));
+        const titleElement: HTMLElement = titleFixture.nativeElement;
+
+        expect(titleElement.innerHTML).toBe(book.title);
+      });
+    });
+
+    describe('When no content is passed', () => {
+      it('renders "n/a"', () => {
+        const titleFixture = fixture.debugElement.query(By.css('mat-card-title'));
+        const titleElement: HTMLElement = titleFixture.nativeElement;
+
+        expect(titleElement.innerHTML).toBe('n/a');
       });
     });
   });
