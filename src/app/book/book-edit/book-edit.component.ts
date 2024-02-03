195feaabdb4e2de8d37e4892421d0849e4efcda7
Christian Schulz <kontakt@schulzolutions.de>
2024-02-03 18:10:03 +0100

    solve--ngrx-use-selectors-with-params

--- src/app/book/book-edit/book-edit.component.ts
+++ src/app/book/book-edit/book-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, filter, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { BookApiService } from '../book-api.service';
 import { Book } from '../models';
@@ -10,6 +10,8 @@ import { MatError, MatFormField } from '@angular/material/form-field';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Store } from '@ngrx/store';
+import { selectBookByIsbn } from '@store/book';
 
 @Component({
   selector: 'ws-book-edit',
@@ -34,12 +36,14 @@ export class BookEditComponent {
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly bookService: BookApiService,
-    private readonly destroyRef: DestroyRef
+    private readonly destroyRef: DestroyRef,
+    private readonly store: Store
   ) {}
 
   @Input({ required: true })
   set isbn(isbn: string) {
-    this.book$ = this.bookService.getByIsbn(isbn).pipe(
+    this.book$ = this.store.select(selectBookByIsbn(isbn)).pipe(
+      filter((book): book is Book => !!book),
       tap(book => {
         this.form.setValue({
           title: book.title,
--- src/app/book/store/book-collection.selectors.ts
+++ src/app/book/store/book-collection.selectors.ts
@@ -4,3 +4,6 @@ import { selectBookFeature } from './book.feature';
 const selectBookCollectionSlice = createSelector(selectBookFeature, feature => feature.bookCollection);
 
 export const selectBookCollection = createSelector(selectBookCollectionSlice, slice => slice.entities);
+
+export const selectBookByIsbn = (isbn: string) =>
+  createSelector(selectBookCollection, books => books.find(book => book.isbn === isbn));
