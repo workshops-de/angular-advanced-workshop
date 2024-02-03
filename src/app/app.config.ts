ff94e996eb461b1b55b8b2df5781a0ba4be6712e
Christian Schulz <kontakt@schulzolutions.de>
2024-02-03 17:24:02 +0100

    solve--ngrx-store-setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideAnimations(), provideRouter(routes, withComponentInputBinding())]
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
