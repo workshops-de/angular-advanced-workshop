fe0b44b86c6e5b9dc765c0573b9b6fe984fdcdbc
Christian Schulz <kontakt@schulzolutions.de>
2024-02-04 15:57:15 +0100

    solve--ngrx-testing-effect-standalone

--- src/app/book/store/book-collection.effects.spec.ts
+++ src/app/book/store/book-collection.effects.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { provideState, provideStore, Store } from '@ngrx/store';
-import { lastValueFrom, of, take } from 'rxjs';
+import { Action, provideState, provideStore, Store } from '@ngrx/store';
+import { lastValueFrom, of, ReplaySubject, take } from 'rxjs';
 import { BookApiService } from '../book-api.service';
 import { bookNa } from '../models';
-import { createBookStart } from './book-collection.actions';
+import { createBookComplete, createBookStart } from './book-collection.actions';
 import { BookCollectionEffects } from './book-collection.effects';
 import { selectBookCollection } from './book-collection.selectors';
 import { provideEffects } from '@ngrx/effects';
 import { bookFeatureName, bookReducers } from './book.feature';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('BookCollection: Effects Integration Test', () => {
   let store: Store;
@@ -38,6 +40,49 @@ describe('BookCollection: Effects Integration Test', () => {
 
       const book$ = lastValueFrom(store.select(selectBookCollection).pipe(take(1)));
       await expectAsync(book$).toBeResolvedTo([book]);
+    });
+  });
+});
+
+describe('BookCollection: Effects', () => {
+  let bookApiMock: jasmine.SpyObj<BookApiService>;
+  let effect: BookCollectionEffects;
+  let actions$: ReplaySubject<Action>;
+
+  beforeEach(() => {
+    bookApiMock = jasmine.createSpyObj<BookApiService>(['create']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: BookApiService, useValue: bookApiMock },
+        BookCollectionEffects,
+        provideMockActions(() => actions$),
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectBookCollection,
+              value: []
+            }
+          ]
+        })
+      ]
+    });
+
+    actions$ = new ReplaySubject();
+    effect = TestBed.inject(BookCollectionEffects);
+  });
+
+  describe('When a book was created successfully', () => {
+    it('dispatches createBookComplete', async () => {
+      const book = bookNa();
+      bookApiMock.create.and.returnValue(of(book));
+
+      actions$.next(createBookStart({ book }));
+
+      const book$ = lastValueFrom(effect.create.pipe(take(1)));
+      await expectAsync(book$).toBeResolvedTo(createBookComplete({ book }));
+
+      expect(bookApiMock.create).toHaveBeenCalledWith(book);
     });
   });
 });
